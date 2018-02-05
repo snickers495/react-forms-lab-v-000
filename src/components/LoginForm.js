a2e4c4d3e38d5f0faf15d05b03d4179e1426bde3
Lisa Lee <snickers495@gmail.com>
2018-02-05 22:28:44 +0000

    automatically backed up by learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -19,7 +19,7 @@ class LoginForm extends React.Component {
 
 
   render() {
-    const isEnabled = {this.}
+    const isEnabled = {this.state.}
     return (
       <form onSubmit={this.props.onSubmit}>
         <div>
