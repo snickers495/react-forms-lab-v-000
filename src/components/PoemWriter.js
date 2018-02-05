5aa6086e7eed7ae3dc1aa7fa752bf58b73254771
Lisa Lee <snickers495@gmail.com>
2018-02-05 22:31:49 +0000

    automatically backed up by learn

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -10,7 +10,7 @@ class PoemWriter extends React.Component {
   render() {
     return (
       <div>
-        <textarea rows="3" cols="60" onChange={}/>
+        <textarea rows="3" cols="60" onChange={this.handleChange}/>
         <div id="poem-validation-error" style={{ color: "red" }}>
           This poem is not written in the right structure!
         </div>
