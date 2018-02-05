9b35856b89f260a47eb4af84ef5a3efef90351f3
Lisa Lee <snickers495@gmail.com>
2018-02-05 22:27:54 +0000

    automatically backed up by learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,28 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      input: "",
+      lessThanMax: true;
+    };
+  }
+
+  handleChange = (event)=> {
+    var input = event.target.value;
+    var maxChars = this.props.maxChars;
+    if (input.length < maxChars) {
+      this.setState({input: input})
+    }
+    else {this.setState({lessThanMax: false})}
   }
 
   render() {
+    const left = this.props.maxChars - this.state.input;
+    const message = this.state.lessThanMax ? `You have ${left} characters left.` : 'Error: over character limit.';
     return (
       <div>
-        <strong>Your message:</strong>
-        <input type="text" />
+        <strong>{message}</strong>
+        <input onChange={this.handleChange} type="text" />
       </div>
     );
   }
