548a0a424efa0b3ebea6bd8f7dae3c9fdc2a7f0a
Lisa Lee <snickers495@gmail.com>
2018-02-05 22:40:41 +0000

    automatically backed up by learn

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -13,7 +13,7 @@ class PoemWriter extends React.Component {
     var input = event.target.value;
     if (input.split('\n').length === 3) {
       var [line1, line2, line3] = input.split('\n');
-      if (line1.split(" ") === 5)
+      if (line1.split(" ") === 5 )
     }
     this.setState({poem: input})
   }
