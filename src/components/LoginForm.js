74a3fec5f11fa8e38d3f899d2519b0fce8a46572
Lisa Lee <snickers495@gmail.com>
2018-02-05 22:30:28 +0000

    automatically backed up by learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -35,7 +35,7 @@ class LoginForm extends React.Component {
           </label>
         </div>
         <div>
-          <button disabled={!} type="submit">Log in</button>
+          <button disabled={!isEnabled} type="submit">Log in</button>
         </div>
       </form>
     );
